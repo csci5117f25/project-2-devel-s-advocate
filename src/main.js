0d81c1d765a18df01d126a1616ad14825054731e
Kira-Balentine <kirabalentine@gmail.com>
2025-11-12 18:11:30 -0600

    Issue #1: Added Font Awesome library to the code.

--- src/main.js
+++ src/main.js
@@ -4,8 +4,26 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/* Import code for Font Awesome was found here:
+      https://docs.fontawesome.com/web/use-with/vue/add-icons */
+
+/* Import the Font Awesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* Import Font Awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* Import all the icons in Free Solid, Free Regular, and Brands styles */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
+
 const app = createApp(App)
 
 app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
