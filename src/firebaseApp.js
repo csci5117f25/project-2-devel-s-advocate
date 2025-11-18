f0fae30e0dbdde91186e63ee8dc66955c64644ab
rivcon <con.rivers@comcast.net>
2025-11-17 23:03:56 -0600

    sanatize

--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -1,15 +1,8 @@
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCjAU10z9IPPfxufiEvbjeb9jRJ-JJtmCA",
-  authDomain: "river853-lec-21.firebaseapp.com",
-  projectId: "river853-lec-21",
-  storageBucket: "river853-lec-21.firebasestorage.app",
-  messagingSenderId: "997834163033",
-  appId: "1:997834163033:web:92935b27f06759a82dc248"
+  //TODO: ADD later
 };
 
 // Initialize Firebase
