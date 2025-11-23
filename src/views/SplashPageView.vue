26c1e5b6058e3bdf5b4f8da88f32f28b79216b02
sumukund <mukund.su@husky.neu.edu>
2025-11-23 11:53:21 -0600

    splashpage

--- src/views/SplashPageView.vue
+++ src/views/SplashPageView.vue
@@ -1,22 +1,12 @@
 <script setup>
-import { ref } from 'vue'
 import { motion } from "motion-v"
+import { ref } from "vue"
+const constraintsRef = ref(null)
 
-const transition = { duration: 4,  ease: "easeInOut" }
-
-const box = {
-    width: '50px',
-    height: '50px',
-    backgroundColor: "#4ff0b7", 
-    borderRadius: '10px',
-    position: "absolute",
-    top: 0,
-    left: 0,
-    offsetPath: `path("M 239 17 C 142 17 48.5 103 48.5 213.5 C 48.5 324 126 408 244 408 C 362 408 412 319 412 213.5 C 412 108 334 68.5 244 68.5 C 154 68.5 102.68 135.079 99 213.5 C 95.32 291.921 157 350 231 345.5 C 305 341 357.5 290 357.5 219.5 C 357.5 149 314 121 244 121 C 174 121 151.5 167 151.5 213.5 C 151.5 260 176 286.5 224.5 286.5 C 273 286.5 296.5 253 296.5 218.5 C 296.5 184 270 177 244 177 C 218 177 197 198 197 218.5 C 197 239 206 250.5 225.5 250.5 C 245 250.5 253 242 253 218.5")`
-}
 </script>
 
 <template>
+
   <div class="flex flex-col mt-32">
     <div class="flex flex-col items-center" id="row-1">
       <div>
@@ -25,26 +15,16 @@ const box = {
         <h2 class="text-2xl text-center font-bold p-4">Mission statement</h2>
       </div>
 
-      <div class="running-animation text-center border border-black h-60 w-72 rounded-xl">
-        <svg xmlns="http://www.w3.org/2000/svg" width="451" height="437">
-            <motion.path
-                d="M 239 17 C 142 17 48.5 103 48.5 213.5 C 48.5 324 126 408 244 408 C 362 408 412 319 412 213.5 C 412 108 334 68.5 244 68.5 C 154 68.5 102.68 135.079 99 213.5 C 95.32 291.921 157 350 231 345.5 C 305 341 357.5 290 357.5 219.5 C 357.5 149 314 121 244 121 C 174 121 151.5 167 151.5 213.5 C 151.5 260 176 286.5 224.5 286.5 C 273 286.5 296.5 253 296.5 218.5 C 296.5 184 270 177 244 177 C 218 177 197 198 197 218.5 C 197 239 206 250.5 225.5 250.5 C 245 250.5 253 242 253 218.5"
-                fill="transparent"
-                stroke-width="12"
-                stroke="var(--hue-6-transparent)"
-                stroke-linecap="round"
-                :initial="{ pathLength: 0.001 }"
-                :animate="{ pathLength: 1 }"
-                :transition="transition"
-            />
-        </svg>
+      <div class="running-animation text-center border border-black h-60 w-72 rounded-xl" style="position: relative">
+        <div ref="constraintsRef" class="constraints">
         <motion.div
-            :style="box"
-            :initial="{ offsetDistance: '0%', scale: 2.5 }"
-            :animate="{ offsetDistance: '100%', scale: 1 }"
-            :transition="transition"
+            drag
+            :dragConstraints="constraintsRef"
+            :dragElastic="0.2"
+            class="box"
         />
     </div>
+      </div>
     </div>
 
     <div class="flex flex-col items-center mt-8" id="row-2">
@@ -53,7 +33,8 @@ const box = {
       </div>
 
       <div class="screenshot text-center border border-black h-36 w-72 rounded-xl">
-        Screenshot 1
+       <motion.img
+       src="../assets/images/image1.png" />
       </div>
     </div>
 
@@ -63,7 +44,8 @@ const box = {
       </div>
 
       <div class="screenshot text-center border border-black h-36 w-72 rounded-xl">
-        Screenshot 2
+        <motion.img
+        src="../assets/images/image2.png" />
       </div>
     </div>
 
@@ -73,7 +55,8 @@ const box = {
       </div>
 
       <div class="screenshot text-center border border-black h-36 w-72 rounded-xl">
-        Screenshot 3
+        <motion.img
+        src="../assets/images/image3.png" />
       </div>
     </div>
 
@@ -119,5 +102,22 @@ const box = {
     height: calc(var(--spacing) * 48);
     width: calc(var(--spacing) * 96);
   }
+}
+.constraints {
+    width: 300px;
+    height: 300px;
+    background-color: var(--hue-1-transparent);
+    border-radius: 10px;
+}
+
+.box {
+    width: 100px;
+    height: 100px;
+    border-radius: 10px;
+
+    background-image: url('../assets/images/P2-Logo\ 3\ tr.png');
+    background-size: cover; 
+    background-position: center;
+    background-repeat: no-repeat;
 }
 </style>
