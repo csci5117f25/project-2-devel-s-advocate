9b229b6d4ee3e42da6322d2ed6e9f73d168977a4
sumukund <mukund.su@husky.neu.edu>
2025-12-03 13:57:17 -0600

    idk if its fixed yet

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,20 @@ import NotFoundView from '../views/NotFoundView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'splashPage', component: SplashPageView, meta: { title: 'Splash Page' } },
+    {
+      path: '/',
+      name: 'splashPage',
+      component: SplashPageView,
+      meta: { title: 'Splash Page' },
+    },
+    {
+      path: '/home',
+      name: 'home',
+      redirect: () => {
+        const auth = getAuth()
+        return auth.currentUser ? '/dashboard' : '/'
+      },
+    },
     {
       path: '/dashboard',
       name: 'dashboard',
