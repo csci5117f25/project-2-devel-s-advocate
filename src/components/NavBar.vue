fdef76c1b23efb5dedb072eef78fdec67a42a8f3
sumukund <mukund.su@husky.neu.edu>
2025-12-07 15:15:39 -0600

    hamburger menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,6 +2,13 @@
 import { RouterLink, useRouter } from 'vue-router'
 import { useCurrentUser } from 'vuefire'
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { ref } from 'vue'
+
+const menuOpen = ref(false);
+
+const toggleMenu = () => {
+  menuOpen.value = !menuOpen.value;
+};
 
 const router = useRouter()
 const user = useCurrentUser()
@@ -28,25 +35,84 @@ const logout = async () => {
       <img src="../assets/images/traceroute_logo.png" alt="tr@ceroute" />
     </RouterLink>
 
+    <div class="hidden md:flex space-x-4 items-center">
+
+        <RouterLink :to="{ name: 'addRun' }">
+          Add Run
+    </RouterLink>
+
+        <RouterLink :to="{ name: 'startRun' }">
+          Start Run
+    </RouterLink>
+          <button
+        v-if="user"
+        @click="logout"
+        class="border border-black font-bold bg-white rounded-xl px-4 py-2 cursor-pointer"
+      >
+        <font-awesome-icon icon="fa-sign-out-alt" />
+        <span class="text-blue-950">Logout</span>
+      </button>
+
+      <button
+        v-else
+        @click="login"
+        class="border border-black font-bold bg-white rounded-xl px-4 py-2 cursor-pointer"
+      >
+        <font-awesome-icon icon="fa-sign-in-alt" />
+        <span class="text-blue-950">Login</span>
+      </button>
+    </div>
+
+      <!-- Hamburger menu-->
+    <button
+      class="md:hidden flex items-center"
+      @click="toggleMenu"
+    >
+      <font-awesome-icon
+        :icon="menuOpen ? 'fa-times' : 'fa-bars'"
+        class="text-2xl"
+      />
+    </button>
+  </nav>
+
+
+
+  <!-- menu div -->
+  <div
+    v-if="menuOpen"
+    class="md:hidden bg-white w-full drop-shadow-xl px-5 py-4 fixed top-24 z-50 flex flex-col space-y-4"
+  >
     <button
       v-if="user"
       @click="logout"
-      class="border border-black font-bold bg-white rounded-xl px-4 py-2 cursor-pointer"
+      class="border border-black font-bold bg-white rounded-xl px-4 py-2 cursor-pointer text-left"
     >
-      <font-awesome-icon icon="fa-sign-out-alt" /><slot class="text-blue-950"> Logout</slot>
+      <font-awesome-icon icon="fa-sign-out-alt" />
+      <span class="ml-2 text-blue-950">Logout</span>
     </button>
+
     <button
       v-else
       @click="login"
-      class="border border-black font-bold bg-white rounded-xl px-4 py-2 cursor-pointer"
+      class="border border-black font-bold bg-white rounded-xl px-4 py-2 cursor-pointer text-left"
     >
-      <font-awesome-icon icon="fa-sign-in-alt" /><slot class="text-blue-950"> Login</slot>
+      <font-awesome-icon icon="fa-sign-in-alt" />
+      <span class="ml-2 text-blue-950">Login</span>
     </button>
-  </nav>
+
+        <RouterLink :to="{ name: 'addRun' }">
+          Add Run
+    </RouterLink>
+
+        <RouterLink :to="{ name: 'startRun' }">
+          Start Run
+    </RouterLink>
+  </div>
 </template>
 
 <style scoped>
 img {
   height: 75px;
 }
+
 </style>
