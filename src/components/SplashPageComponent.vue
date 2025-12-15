4e43c194fa8045d4493d6492b6f8e74641474dd3
sumukund <mukund.su@husky.neu.edu>
2025-12-15 09:46:49 -0600

    good

--- src/components/SplashPageComponent.vue
+++ src/components/SplashPageComponent.vue
@@ -2,28 +2,24 @@
 import { ref } from 'vue'
 import { useLoop, useLoader } from '@tresjs/core'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { TextureLoader } from 'three'
+import { Sparkles } from '@tresjs/cientos'
 
 const modelRef = ref()
 
-const { state: model } = useLoader(
-  GLTFLoader,
-  '/models/scene.gltf'
-)
+const { state: model } = useLoader(GLTFLoader, 'src/assets/images/P2-Logo 3 tr.png')
 const { onBeforeRender } = useLoop()
 
 onBeforeRender(({ elapsed }) => {
-    if (modelRef.value) {
+  if (modelRef.value) {
     const scale = 1 + Math.sin(elapsed * 2) * 0.7
     modelRef.value.scale.set(scale, scale, scale)
-    modelRef.value.rotation.x = elapsed * 0.5
+    modelRef.value.rotation.x = elapsed * 0.2
     modelRef.value.rotation.y = elapsed * 0.3
     modelRef.value.position.y = Math.sin(elapsed * 2) * 0.9
-    modelRef.value.position.x = Math.sin(elapsed * 2) * 0.2
   }
 })
-
 </script>
+
 
 <template>
   <TresPerspectiveCamera
@@ -35,6 +31,7 @@ onBeforeRender(({ elapsed }) => {
     v-if="model?.scene"
     ref="modelRef"
     :object="model.scene"
-    :position="[0, 0, 0]"
   />
+  <TresAmbientLight :intensity="1" />
 </template>
+
--- src/views/SplashPageView.vue
+++ src/views/SplashPageView.vue
@@ -2,7 +2,7 @@
 import { useRouter } from 'vue-router'
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { motion } from 'motion-v'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 const constraintsRef = ref(null)
 import SplashPageComponent from '@/components/SplashPageComponent.vue'
 import { TresCanvas } from '@tresjs/core'
@@ -14,44 +14,63 @@ const login = async () => {
   await signInWithPopup(auth, provider)
   router.push({ name: 'dashboard' })
 }
+
+const words = ['run', 'bike', 'walk']
+const currentWord = ref(words[0])
+let index = 0
+
+onMounted(() => {
+  setInterval(() => {
+    index = (index + 1) % words.length
+    currentWord.value = words[index]
+  }, 2000)
+})
+
 </script>
 
 <template>
   <div class="flex flex-col items-center mt-32 m-12 text-off-white drop-shadow-xl/50">
     <div class="flex flex-col items-center" id="row-1">
-      <div class="half-width">
-        <h1 class="text-3xl text-center text-orange-salmon font-bold p-4">Welcome!</h1>
-
-        <h2 class="text-2xl text-center font-bold p-4">
-          Built for those who love to exercise and track their progress!
-        </h2>
-      </div>
-
-      <div
-        class="half-width running-animation text-center border-6 border-orange-salmon rounded-xl"
-        style="position: relative; height: 250px; width: 250px"
-      >
-        <TresCanvas clearColor="#7d4e57">
-          <SplashPageComponent />
-        </TresCanvas>
-      </div>
     </div>
 
-    <div id="animation-set" class="flex flex-row justify-between mt-12">
-      <div class="animation">
-        <img src="../assets/gifs/walking_animation.gif" class="h-60" />
-      </div>
-      <div class="animation">
-        <img src="../assets/gifs/running_animation.gif" class="h-60" />
-      </div>
-      <div class="animation">
-        <img src="../assets/gifs/biking_animation.gif" class="h-75" />
-      </div>
+<div id="animation-set" class="flex flex-row justify-between mt-12">
+  <div class="animation">
+    <img src="../assets/gifs/walking_animation.gif" class="h-60" />
+  </div>
+  <div class="half-width flex justify-center">
+    <div class="relative flex items-center justify-center h-60 w-full">
+      <img
+        src="../assets/gifs/running_animation.gif"
+        class="absolute inset-0 mx-auto h-60 opacity-40 -z-10"
+      />
+
+     <h2 class="text-4xl text-center p-4 z-10 racing-sans-one-regular">
+    Built for those who love to&nbsp;
+
+    <motion.span
+      :key="currentWord"
+      class="inline-block font-bold text-orange-salmon"
+      :initial="{ y: -30, opacity: 0 }"
+      :animate="{ y: 0, opacity: 1 }"
+      :exit="{ y: 30, opacity: 0 }"
+      :transition="{ duration: 0.4, ease: 'easeOut' }"
+    >
+      {{ currentWord }}
+    </motion.span>
+    !
+  </h2>
+
     </div>
+  </div>
+
+  <div class="animation">
+    <img src="../assets/gifs/biking_animation.gif" class="h-75" />
+  </div>
+</div>
 
     <div class="flex flex-col items-center m-8" id="row-2">
       <div class="half-width">
-        <h2 class="text-2xl text-center font-bold p-4">Fun User Features</h2>
+        <h2 class="text-2xl text-center p-4 racing-sans-one-regular">Advanced Tracking so you can hit your goals!</h2>
       </div>
 
       <div class="half-width screenshot text-center border-6 border-orange-salmon rounded-xl">
@@ -61,8 +80,8 @@ const login = async () => {
 
     <div class="flex flex-col items-center m-8" id="row-3">
       <div class="half-width">
-        <h2 class="text-2xl text-center font-bold p-4">
-          Advanced Tracking so you can hit your goals!
+        <h2 class="text-2xl text-center racing-sans-one-regular p-4">
+          Add comments to keep track of your feelings!
         </h2>
       </div>
 
@@ -71,27 +90,15 @@ const login = async () => {
       </div>
     </div>
 
-    <div class="flex flex-col items-center m-8" id="row-4">
-      <div class="half-width">
-        <h2 class="text-2xl text-center font-bold p-4">
-          Add comments to your runs to keep track of your feelings!
-        </h2>
-      </div>
-
-      <div class="half-width screenshot text-center border-6 border-orange-salmon rounded-xl">
-        <img src="../assets/images/image3.png" class="rounded-xl" />
-      </div>
-    </div>
-
     <div
       id="row-5"
       class="flex flex-col text-center items-center justify-center p-4 mt-10 bottom-0 z-1000 w-full"
     >
-      <h2 class="text-2xl p-4">Join today and become part of our running family!</h2>
+      <h2 class="text-2xl racing-sans-one-regular p-4">Join today and become part of our running family!</h2>
 
       <motion.button
         @click="login"
-        class="text-center text-orange-salmon bg-off-white active:bg-lightgray rounded-xl px-4 py-2 mx-4 my-2 cursor-pointer"
+        class="text-center text-orange-salmon bg-off-white active:bg-lightgray rounded-xl px-4 py-2 mx-4 my-2 cursor-pointer racing-sans-one-regular"
         :whileHover="{ scale: 1.15, rotate: 3 }"
         :whileTap="{ scale: 0.9, rotate: -5 }"
         :transition="{ type: 'spring', stiffness: 400, damping: 17 }"
@@ -103,6 +110,8 @@ const login = async () => {
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Racing+Sans+One&display=swap');
+
 @media (min-width: 1024px) {
   #row-1 {
     flex-direction: row;
@@ -152,4 +161,10 @@ const login = async () => {
   background-position: center;
   background-repeat: no-repeat;
 }
+
+.racing-sans-one-regular {
+  font-family: "Racing Sans One", sans-serif;
+  font-weight: 400;
+}
+
 </style>
